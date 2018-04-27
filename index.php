cd5da39a625a88c069b9ca9e9cebfd0ea8e4aa6e
Gus Liakos <Gliakos@cnm.edu>
2018-04-27 08:43:54 -0600

    lets do this!

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gusli
+ * Date: 4/27/2018
+ * Time: 8:23 AM
+ */ ?>
+<!DOCTYPE html>
+<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<link rel = "stylesheet" type="text/css" href=" "
+			<title>Ipsum</title>
+
+	</head>
+	<body>
+		<div>
+			<p>
+				Not if I weaken first. Computer, lights up! Well, that's certainly good to know. Yesterday I did not know
+				how to eat gagh. Sorry, Data. This is not about revenge. This is about justice. and attack the Romulans. We
+				have a saboteur aboard. I'm afraid I still don't understand, sir. Some days you get the bear, and some days
+				the bear gets you. A surprise party? Mr. Worf, I hate surprise parties. I would *never* do that to you. This
+				should be interesting. About four years. I got tired of hearing how young I looked. My oath is between
+				Captain Kargan and myself. Your only concern is with how you obey my orders. Or do you prefer the rank of
+				prisoner to that of lieutenant? We finished our first sensor sweep of the neutral zone. Now, how the hell do
+				we defeat an enemy that knows us better than we know ourselves? Well, I'll say this for him - he's sure of
+				himself. The Federation's gone; the Borg is everywhere! Wouldn't that bring about chaos?
+			</p>
+		</div>
+		<div>
+			<p class="yeezy">
+				You are now watching the throne, don't let me get in my zone. You tellin' me people don't look at Kanye West
+				like the glitch? I really do believe that the world can be saved through design, and everything needs to
+				actually be 'architected.' Because I sit back and think 'Am I the only one that's not crazy? I'm a creative
+				genius and there's no other way to word it. This one Corbusier lamp was, like, my greatest inspiration.
+				Nobody can tell me where I can and can't go. Man, I'm the number one living and breathing rock star.
+			</p>
+			<p class="threestars">
+				Let me begin baby, my name is Bronsolino. Eat steaks off a cold plate. listening to Coldplay. Hide spinach
+				like a doomsday prepper. Every Sunday I'm in Tuesday leather. My beard look like Uday and Qusay. Zero to 60
+				in a Porsche like a cheetah. The moon's reflections off the lake hit me. All I see is C-notes, silk shirts
+				at the casino. The face is silky like a tablecloth. My shorty gallop in the morning on the beach like a
+				Chilean horse. I fixed the game between Georgia Southern and Grambling. I'm sitting at the outdoor desk with
+				my pants off.
+			</p>
+		</div>
+		<p class="ninetieswererad">
+			Keep your heart, Three Stacks, keep your heart. Play your part. On the oak tree, I hope we feel like this
+			forever. Forever ever? But I know you wanted that 808. Can you feel that B-A-S-S bass? Ah ha, hush that fuss.
+			Everybody move to the back of the bus. Do you wanna bump and slump with us? We the type of people that make the
+			club get crunk. As the plot thickens it gives me the dickens, reminiscent of Charles. When I met my
+			SpottieOttieDopalicious angel, I can remember the damn thing like yesterday. Sir Lucious gots gator belts and
+			patty melts and Monte Carlos. This is my story, this is my song and to them rudypoos don't attempt to try this
+			at home. Chonkyfire, spiced with rock n' roll. Indubitably, piper pied. And if I see you in the streets I'd dap
+			you down like you was Greg. YKK on your zipper.
+		</p>
+		<div class="martin">
+			<p class = "loosygoosy">Kendrick have a dream. I am a sinner who's probably going to sin again. Playstation and some drank.
+				Technology bought my soul. Got me breathing with dragons. I crack the eggs in your basket. I'm Marilyn
+				Manson with madness, now just imagine the magic. Start up that Maserati and 'Vroom! Vroom' I'm racing. I got
+				25 lighters on my dresser. I can feel your energy from two planets away. I got my drink, I got my music. I
+				would share them both today. I can feel the changes. I can feel the people around me just want to be
+				famous.</p>
+		</div>
+		<p id = "williamtriker">
+			Not if I weaken first. Computer, lights up! Well, that's certainly good to know. Yesterday I did not know
+			how to eat gagh. Sorry, Data. This is not about revenge. This is about justice. and attack the Romulans. We
+			have a saboteur aboard. I'm afraid I still don't understand, sir. Some days you get the bear, and some days
+			the bear gets you. A surprise party? Mr. Worf, I hate surprise parties. I would *never* do that to you. This
+			should be interesting. About four years. I got tired of hearing how young I looked.
+		</p>
+	</body>
+</html>
