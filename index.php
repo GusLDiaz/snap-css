e70d26adeedd8009794f5413d94b1f2bfc1e5222
Gus Liakos <Gliakos@cnm.edu>
2018-04-27 08:52:18 -0600

    add sketchy in-line

--- index.php
+++ index.php
@@ -7,26 +7,22 @@
  */ ?>
 <!DOCTYPE html>
 <html lang="en">
-		<head>
-			<meta charset="UTF-8">
-			<link rel = "stylesheet" type="text/css" href="ipsum.css">
-			<title>Ipsum</title>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="ipsum.css">
+		<title>Ipsum</title>
 
 	</head>
 	<body>
-		<div>
-			<p>
-				Not if I weaken first. Computer, lights up! Well, that's certainly good to know. Yesterday I did not know
-				how to eat gagh. Sorry, Data. This is not about revenge. This is about justice. and attack the Romulans. We
-				have a saboteur aboard. I'm afraid I still don't understand, sir. Some days you get the bear, and some days
-				the bear gets you. A surprise party? Mr. Worf, I hate surprise parties. I would *never* do that to you. This
-				should be interesting. About four years. I got tired of hearing how young I looked. My oath is between
+		<span style="font-stretch: ultra-expanded">
+			<p class="oath">
+				My oath is between
 				Captain Kargan and myself. Your only concern is with how you obey my orders. Or do you prefer the rank of
 				prisoner to that of lieutenant? We finished our first sensor sweep of the neutral zone. Now, how the hell do
 				we defeat an enemy that knows us better than we know ourselves? Well, I'll say this for him - he's sure of
 				himself. The Federation's gone; the Borg is everywhere! Wouldn't that bring about chaos?
 			</p>
-		</div>
+		</span>
 		<div>
 			<p class="yeezy">
 				You are now watching the throne, don't let me get in my zone. You tellin' me people don't look at Kanye West
@@ -55,14 +51,15 @@
 			you down like you was Greg. YKK on your zipper.
 		</p>
 		<div class="martin">
-			<p class = "loosygoosy">Kendrick have a dream. I am a sinner who's probably going to sin again. Playstation and some drank.
+			<p class="loosygoosy">Kendrick have a dream. I am a sinner who's probably going to sin again. Playstation and
+				some drank.
 				Technology bought my soul. Got me breathing with dragons. I crack the eggs in your basket. I'm Marilyn
 				Manson with madness, now just imagine the magic. Start up that Maserati and 'Vroom! Vroom' I'm racing. I got
 				25 lighters on my dresser. I can feel your energy from two planets away. I got my drink, I got my music. I
 				would share them both today. I can feel the changes. I can feel the people around me just want to be
 				famous.</p>
 		</div>
-		<p id = "williamtriker">
+		<p id="williamtriker">
 			Not if I weaken first. Computer, lights up! Well, that's certainly good to know. Yesterday I did not know
 			how to eat gagh. Sorry, Data. This is not about revenge. This is about justice. and attack the Romulans. We
 			have a saboteur aboard. I'm afraid I still don't understand, sir. Some days you get the bear, and some days
